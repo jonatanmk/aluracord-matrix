75e2d2a7d5437127c011efb23e45440bc45dadd1
jonatanmk <jonatan.mk@outlook.com>
2022-01-30 18:17:18 -0300

    Atualização aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from "react";
+import { useRouter } from 'next/router';
 import appConfig from"../config.json"
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -59,11 +32,13 @@ function Titulo(props) {
 export default HomePage;*/
 
 export default function PaginaInicial() {
-    const username = 'JonatanMK';
+    /* const username = 'JonatanMK'; */
+    const [username, setUsername] = React.useState('JonatanMK');
+    const roteamento = useRouter();
   
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -90,17 +65,41 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                console.log('Algue submeteu o form')
+                roteamento.push('/chat')
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
               }}
             >
-              <Titulo tag="h2">Boas vindas de volta!</Titulo>
+              <Titulo tag="h2">Boas vindas de porra!</Titulo>
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+              
+{/*               <input 
+                type="text"
+                value={username}
+                onChange={function (event) {
+                  console.log('usuario digitou', event.target.value)
+                  //onde está o valor?
+                  const valor = event.target.value;
+                  //trocar o valor da variavel
+                  setUsername(valor);                
+                }}
+              /> */}
               <TextField
+               value={username}
+               onChange={function (event) {
+                 console.log('usuario digitou', event.target.value)
+                 //onde está o valor?
+                 const valor = event.target.value;
+                 //trocar o valor da variavel
+                 setUsername(valor);                
+               }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
